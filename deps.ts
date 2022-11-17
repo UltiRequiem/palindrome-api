6bae3de1cd816365ce037e642c5f2cc03888a071
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-11-16 20:29:57 -0500

    :arrow_up: Bump Oak

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export {
   Application,
   Router,
   type RouterMiddleware,
-} from "https://deno.land/x/oak@v10.5.1/mod.ts";
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
